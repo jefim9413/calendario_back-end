d0a36efe39bdcc9222d0a039152af247cf151e25
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-20 20:05:17 -0300

    finalizando controller create task

--- src/modules/tasks/useCases/createTask/CreateTaskController.ts
+++ src/modules/tasks/useCases/createTask/CreateTaskController.ts
@@ -1,7 +1,20 @@
 import { Request, Response } from "express";
+import { CreateTaskUseCase } from "./CreateTaskUseCase";
 
 class CreateTaskController {
-  async handle(request: Request, response: Response) {}
+  async handle(request: Request, response: Response) {
+    const { title, description, date, duracao } = request.body;
+    const createTaskUseCase = new CreateTaskUseCase();
+
+    const result = await createTaskUseCase.execute({
+      title,
+      description,
+      date,
+      duracao,
+    });
+
+    return response.json(result);
+  }
 }
 
 export { CreateTaskController };
