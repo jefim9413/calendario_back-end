e051bbb6b6cb1f2c28cd5a060eccbfad4535bee3
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-20 16:27:55 -0300

    finalizando useCase de create Task

--- src/modules/tasks/useCases/createTask/CreateTaskUseCase.ts
+++ src/modules/tasks/useCases/createTask/CreateTaskUseCase.ts
@@ -1,3 +1,24 @@
-class CreateTaskUseCase {}
+import { prisma } from "../../../../database/prismaClient";
+interface ICreateTesk {
+  title: string;
+  description: string;
+  date: string;
+  duracao: string;
+}
+
+class CreateTaskUseCase {
+  async execute({ title, description, date, duracao }: ICreateTesk) {
+    const task = await prisma.tarefa.create({
+      data: {
+        title,
+        description,
+        date,
+        duracao,
+      },
+    });
+
+    return task;
+  }
+}
 
 export { CreateTaskUseCase };
