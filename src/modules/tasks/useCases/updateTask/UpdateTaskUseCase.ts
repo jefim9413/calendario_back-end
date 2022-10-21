eb84e209e757b9b8143461f18b97ee7fb2d026ac
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-21 10:27:42 -0300

    realizando update de task

--- src/modules/tasks/useCases/updateTask/UpdateTaskUseCase.ts
+++ src/modules/tasks/useCases/updateTask/UpdateTaskUseCase.ts
@@ -1,3 +1,27 @@
-class UpdateTaskUseCase {}
+import { prisma } from "../../../../database/prismaClient";
+
+interface IUpdateTask {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+  duracao: string;
+}
+
+class UpdateTaskUseCase {
+  async execute({ id, date, description, duracao, title }: IUpdateTask) {
+    const task = await prisma.task.update({
+      where: { id },
+      data: {
+        title,
+        description,
+        date,
+        duracao,
+      },
+    });
+
+    return task;
+  }
+}
 
 export { UpdateTaskUseCase };
