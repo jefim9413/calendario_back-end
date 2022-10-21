9fc0b779d383748852f899b47aef0c3554329a2c
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-21 15:10:39 -0300

    Adicionando rota de busca de tarefas por título

--- src/routes/Task.routes.ts
+++ src/routes/Task.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { CreateTaskController } from "../modules/tasks/useCases/createTask/CreateTaskController";
 import { DeleteTaskController } from "../modules/tasks/useCases/deleteTask/DeleteTaskController";
+import { FindByNameTaskController } from "../modules/tasks/useCases/findByNameTask /FindByNameTaskController";
 import { ListTaskController } from "../modules/tasks/useCases/listTask/ListTaskController";
 import { UpdateTaskController } from "../modules/tasks/useCases/updateTask/UpdateTaskController";
 
@@ -9,9 +10,12 @@ const createTaskController = new CreateTaskController();
 const listTaskController = new ListTaskController();
 const deleteTaskController = new DeleteTaskController();
 const updateTaskController = new UpdateTaskController();
+const findByNameTaskController = new FindByNameTaskController();
 
 routesTask.post("/", createTaskController.handle);
 routesTask.get("/", listTaskController.handle);
 routesTask.delete("/:id", deleteTaskController.handle);
 routesTask.put("/:id", updateTaskController.handle);
+routesTask.get("/titulo", findByNameTaskController.handle);
+
 export { routesTask };
