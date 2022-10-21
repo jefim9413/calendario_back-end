f0129ca4d4b0d24c0cf6aa682dce300d0665b780
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-21 14:01:02 -0300

    realizando busca de task pelo titulo

--- src/modules/tasks/useCases/findByNameTask /FindByNameTaskUseCase.ts
+++ src/modules/tasks/useCases/findByNameTask /FindByNameTaskUseCase.ts
@@ -1,3 +1,18 @@
-class FindByNameTaskUseCase {}
+import { prisma } from "../../../../database/prismaClient";
+
+class FindByNameTaskUseCase {
+  async execute(title: string) {
+    const result = await prisma.task.findMany({
+      where: {
+        title: {
+          equals: title,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    return result;
+  }
+}
 
 export { FindByNameTaskUseCase };
