3d860a001a34c030429d5f45968d6752a3cf8b37
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-21 09:09:10 -0300

    tratando erros

--- src/modules/tasks/useCases/deleteTask/DeleteTaskUseCase.ts
+++ src/modules/tasks/useCases/deleteTask/DeleteTaskUseCase.ts
@@ -1,3 +1,17 @@
-class DeleteTaskUseCase {}
+import { prisma } from "../../../../database/prismaClient";
+
+class DeleteTaskUseCase {
+  async execute(id: string) {
+    const task = await prisma.task.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!task) {
+      throw new Error("Task not found");
+    }
+  }
+}
 
 export { DeleteTaskUseCase };
