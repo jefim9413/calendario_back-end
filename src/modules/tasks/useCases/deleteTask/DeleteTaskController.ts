0a0ebb1b995bc90ec6cdb44ce2fc061e0edabdad
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-21 09:09:57 -0300

    finalizando função handle

--- src/modules/tasks/useCases/deleteTask/DeleteTaskController.ts
+++ src/modules/tasks/useCases/deleteTask/DeleteTaskController.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from "express";
+import { DeleteTaskUseCase } from "./DeleteTaskUseCase";
 
 class DeleteTaskController {
-  async handle(request: Request, response: Response) {}
+  async handle(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const deleteTaskUseCase = new DeleteTaskUseCase();
+
+    const task = await deleteTaskUseCase.execute(id);
+
+    return response.status(200).json(task);
+  }
 }
 
 export { DeleteTaskController };
