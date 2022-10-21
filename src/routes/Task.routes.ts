3edc8b9df4f9ca9ba753515ae104d9a322b11835
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-21 09:08:06 -0300

    adicionando rota de delete task

--- src/routes/Task.routes.ts
+++ src/routes/Task.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { CreateTaskController } from "../modules/tasks/useCases/createTask/CreateTaskController";
+import { DeleteTaskController } from "../modules/tasks/useCases/deleteTask/DeleteTaskController";
 import { ListTaskController } from "../modules/tasks/useCases/listTask/ListTaskController";
 
 const routesTask = Router();
 const createTaskController = new CreateTaskController();
 const listTaskController = new ListTaskController();
-
+const deleteTaskController = new DeleteTaskController();
 routesTask.post("/", createTaskController.handle);
 routesTask.get("/", listTaskController.handle);
+routesTask.delete("/:id", deleteTaskController.handle);
 export { routesTask };
