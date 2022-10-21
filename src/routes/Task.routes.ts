8263cdb8056c2723292f931acbb0815a80c733d8
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-21 10:36:43 -0300

    adicionando rota de update para Task

--- src/routes/Task.routes.ts
+++ src/routes/Task.routes.ts
@@ -2,12 +2,16 @@ import { Router } from "express";
 import { CreateTaskController } from "../modules/tasks/useCases/createTask/CreateTaskController";
 import { DeleteTaskController } from "../modules/tasks/useCases/deleteTask/DeleteTaskController";
 import { ListTaskController } from "../modules/tasks/useCases/listTask/ListTaskController";
+import { UpdateTaskController } from "../modules/tasks/useCases/updateTask/UpdateTaskController";
 
 const routesTask = Router();
 const createTaskController = new CreateTaskController();
 const listTaskController = new ListTaskController();
 const deleteTaskController = new DeleteTaskController();
+const updateTaskController = new UpdateTaskController();
+
 routesTask.post("/", createTaskController.handle);
 routesTask.get("/", listTaskController.handle);
 routesTask.delete("/:id", deleteTaskController.handle);
+routesTask.put("/:id", updateTaskController.handle);
 export { routesTask };
