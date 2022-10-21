e772565d5aefca61e6385059f14d5c25a9460a3a
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-21 09:30:20 -0300

    tratando as exceções lançadas com o throw

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import "express-async-errors";
+import express, { NextFunction, Request, Response } from "express";
 import { router } from "./routes";
 
 const app = express();
@@ -6,6 +7,19 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  }
+);
 
 app.listen(3333, () => {
   console.log("🚀 Server is Running 🚀");
