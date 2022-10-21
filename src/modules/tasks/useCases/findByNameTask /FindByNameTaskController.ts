a070ac8b82f9e465f92198e1dfbcf873c718c9b7
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-21 15:09:28 -0300

    finalizando função handle do controller

--- src/modules/tasks/useCases/findByNameTask /FindByNameTaskController.ts
+++ src/modules/tasks/useCases/findByNameTask /FindByNameTaskController.ts
@@ -1,5 +1,16 @@
+import { Request, Response } from "express";
+import { FindByNameTaskUseCase } from "./FindByNameTaskUseCase";
+
 class FindByNameTaskController {
-  async handle() {}
+  async handle(request: Request, response: Response) {
+    const { title } = request.body;
+
+    const findByNameTaskUseCase = new FindByNameTaskUseCase();
+
+    const result = await findByNameTaskUseCase.execute(title);
+
+    return response.json(result);
+  }
 }
 
 export { FindByNameTaskController };
