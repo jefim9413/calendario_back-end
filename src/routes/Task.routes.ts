2a4c3c4831dcd5186d526626d69af53e91395c32
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-20 21:14:03 -0300

    adicionando listar tasks a rotas

--- src/routes/Task.routes.ts
+++ src/routes/Task.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { CreateTaskController } from "../modules/tasks/useCases/createTask/CreateTaskController";
+import { ListTaskController } from "../modules/tasks/useCases/listTask/ListTaskController";
 
 const routesTask = Router();
 const createTaskController = new CreateTaskController();
+const listTaskController = new ListTaskController();
 
 routesTask.post("/", createTaskController.handle);
-
+routesTask.get("/", listTaskController.handle);
 export { routesTask };
