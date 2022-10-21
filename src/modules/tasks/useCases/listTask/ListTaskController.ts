51660bc0d430f7e4775bade9622536098350239b
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-21 08:18:18 -0300

    finalizando controller de

--- src/modules/tasks/useCases/listTask/ListTaskController.ts
+++ src/modules/tasks/useCases/listTask/ListTaskController.ts
@@ -1,3 +1,14 @@
-class ListTaskController {}
+import { Request, Response } from "express";
+import { ListTaskUseCase } from "./ListTaskUseCase";
+
+class ListTaskController {
+  async handle(request: Request, response: Response) {
+    const listTaskUseCase = new ListTaskUseCase();
+
+    const tasks = await listTaskUseCase.execute();
+
+    return response.json(tasks);
+  }
+}
 
 export { ListTaskController };
