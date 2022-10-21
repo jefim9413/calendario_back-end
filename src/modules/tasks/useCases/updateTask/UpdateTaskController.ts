440b6d0c13dd3b00d1abbe95323343f3120b398a
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-21 10:35:39 -0300

    finalizando controller de update

--- src/modules/tasks/useCases/updateTask/UpdateTaskController.ts
+++ src/modules/tasks/useCases/updateTask/UpdateTaskController.ts
@@ -1,7 +1,22 @@
 import { Request, Response } from "express";
+import { UpdateTaskUseCase } from "./UpdateTaskUseCase";
 
 class UpdateTaskController {
-  async handle(request: Request, response: Response) {}
+  async handle(request: Request, response: Response) {
+    const { id } = request.params;
+    const { title, description, date, duracao } = request.body;
+    const updateTaskUseCase = new UpdateTaskUseCase();
+
+    const task = await updateTaskUseCase.execute({
+      id,
+      title,
+      description,
+      date,
+      duracao,
+    });
+
+    return response.json(task);
+  }
 }
 
 export { UpdateTaskController };
