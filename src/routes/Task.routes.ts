a54e98b10802b285beb6be071e72468b9ce18921
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-25 23:40:41 -0300

    Adicionando rota de busca de tarefas por id

--- src/routes/Task.routes.ts
+++ src/routes/Task.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { CreateTaskController } from "../modules/tasks/useCases/createTask/CreateTaskController";
 import { DeleteTaskController } from "../modules/tasks/useCases/deleteTask/DeleteTaskController";
 import { FindByNameTaskController } from "../modules/tasks/useCases/findByNameTask /FindByNameTaskController";
+import { FindByOneTaskController } from "../modules/tasks/useCases/findByOneTask/FindByOneController";
 import { ListTaskController } from "../modules/tasks/useCases/listTask/ListTaskController";
 import { UpdateTaskController } from "../modules/tasks/useCases/updateTask/UpdateTaskController";
 
@@ -11,9 +12,11 @@ const listTaskController = new ListTaskController();
 const deleteTaskController = new DeleteTaskController();
 const updateTaskController = new UpdateTaskController();
 const findByNameTaskController = new FindByNameTaskController();
+const findByOneTaskController = new FindByOneTaskController();
 
 routesTask.post("/", createTaskController.handle);
 routesTask.get("/", listTaskController.handle);
+routesTask.get("/:id", findByOneTaskController.handle);
 routesTask.delete("/:id", deleteTaskController.handle);
 routesTask.put("/:id", updateTaskController.handle);
 routesTask.get("/titulo", findByNameTaskController.handle);
