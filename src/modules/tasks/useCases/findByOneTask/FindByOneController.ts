1f1eb7107e792a041b9c6edfbcd49e8c760f6554
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-25 23:41:40 -0300

    finalizando controller FindByOn

--- src/modules/tasks/useCases/findByOneTask/FindByOneController.ts
+++ src/modules/tasks/useCases/findByOneTask/FindByOneController.ts
@@ -1,3 +1,16 @@
-class FindByOneTaskController {}
+import { Request, Response } from "express";
+import { FindByOneTaskUseCase } from "./FindByOneTaskUseCase";
+
+class FindByOneTaskController {
+  async handle(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const findByOneTaskUseCase = new FindByOneTaskUseCase();
+
+    const result = await findByOneTaskUseCase.execute(id);
+
+    return response.json(result);
+  }
+}
 
 export { FindByOneTaskController };
