189f63fd0bc341610a7f7d63679c48211e88cfd2
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-25 23:41:19 -0300

    finalizando useCase findByOne

--- src/modules/tasks/useCases/findByOneTask/FindByOneTaskUseCase.ts
+++ src/modules/tasks/useCases/findByOneTask/FindByOneTaskUseCase.ts
@@ -1,3 +1,18 @@
-class FindByOneTaskUseCase {}
+import { prisma } from "../../../../database/prismaClient";
+
+class FindByOneTaskUseCase {
+  async execute(id: string) {
+    const result = await prisma.task.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!result) {
+      throw new Error("There is no task with that id");
+    }
+
+    return result;
+  }
+}
 
 export { FindByOneTaskUseCase };
